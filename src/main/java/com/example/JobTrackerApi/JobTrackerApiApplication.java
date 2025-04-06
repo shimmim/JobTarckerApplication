1fa278d018eb3182429d3382fd90c0b81f337e1c
shamimmiroliaei <shamim.miroliaei@stud.s>
2025-04-06 14:02:24 +0200

    Removed JobTarckerApplication from staging and added changes

---
+++ src/main/java/com/example/JobTrackerApi/JobTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.JobTrackerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTrackerApiApplication.class, args);
+	}
+
+}
